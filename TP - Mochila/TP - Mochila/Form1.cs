fa6b29fa41669d7461b6fbb32d20a3df76903e14
Juan Cruz Indurain <juancindurain@gmail.com>
2019-09-18 11:04:37 -0300

    Add files via upload

---
+++ TP - Mochila/TP - Mochila/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP___Mochila
+{
+    public partial class Form1 : Form
+    {
+        List<cObjeto> Objetos = new List<cObjeto>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TP - Mochila/TP - Mochila/Mochila.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP___Mochila
+{
+    class cMochila
+    {
+        public float Capacidad { get; set; }
+
+        public float PesoActual { get; set; }
+
+        public int ValorActual { get; set; }
+    }
+}
